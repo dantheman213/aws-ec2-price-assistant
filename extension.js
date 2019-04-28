091ee82562dc35848ab356dd7d373f89d1deef80
Daniel Gillespie <daniel.gillespie@pluto.tv>
2019-04-28 08:26:43 -0700

    fixed issues with currency formatting and decimal places. adding version 1.1 to manifest

--- extension.js
+++ extension.js
@@ -2,43 +2,74 @@ const hoursInMonth = 730.001;
 const hoursInYear = 8760;
 
 let convertedElementCount = 0;
+let inProgress = false;
 
 function updatePricing() {
-    console.log('Reading Page Data For AWS Price Assistant...');
-    const elements = document.getElementsByTagName('*');
+    if (!inProgress) {
+        inProgress = true;
 
-    for (var i = 0; i < elements.length; i++) {
-        const element = elements[i];
+        console.log('Reading Page Data For AWS Price Assistant...');
+        const elements = document.getElementsByTagName('*');
 
-        for (var j = 0; j < element.childNodes.length; j++) {
-            const node = element.childNodes[j];
+        for (var i = 0; i < elements.length; i++) {
+            const element = elements[i];
 
-            if (node.nodeType === 3) {
-                const text = node.nodeValue.toLowerCase();
-                if (text.indexOf('per hour') > -1) {
-                    if (/(?=.*\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|0)?(\.\d{1,8})? per hour$/.test(text)) {
-                        convertedElementCount++;
+            for (var j = 0; j < element.childNodes.length; j++) {
+                const node = element.childNodes[j];
 
-                        const hourlyPrice = parseFloat(text.substring(1, text.indexOf(' ')));
-                        const maxDecimalCount = (hourlyPrice.toString().length - hourlyPrice.toString().indexOf('.') - 1);
+                if (node.nodeType === 3) {
+                    const text = node.nodeValue.toLowerCase();
+                    if (text.indexOf('per hour') > -1) {
+                        if (/(?=.*\d)^\$?(([1-9]\d{0,2}(,\d{3})*)|0)?(\.\d{1,8})? per hour$/.test(text)) {
+                            convertedElementCount++;
 
-                        const monthlyPrice = parseFloat((hourlyPrice * hoursInMonth).toFixed(maxDecimalCount));
-                        const yearlyPrice = parseFloat((hourlyPrice * hoursInYear).toFixed(maxDecimalCount));
+                            const hourlyPrice = parseFloat(text.substring(1, text.indexOf(' ')));
+                            let maxDecimalCount = (hourlyPrice.toString().length - hourlyPrice.toString().indexOf('.') - 1);
+                            if (maxDecimalCount < 2)
+                                maxDecimalCount = 2;
 
-                        let payloadHTML = `<div style="text-align: left;">$${hourlyPrice} <strong>per hour</strong><br>`;
-                        payloadHTML += `$${monthlyPrice} <strong>per month</strong><br>`;
-                        payloadHTML += `$${yearlyPrice} <strong>per year</strong><br></div>`;
+                            const monthlyPrice = parseFloat((hourlyPrice * hoursInMonth).toFixed(maxDecimalCount));
+                            const yearlyPrice = parseFloat((hourlyPrice * hoursInYear).toFixed(maxDecimalCount));
 
-                        element.innerHTML = payloadHTML;
+                            let payloadHTML = `<div style="text-align: left;">${convertCurrencyToPrettyFormat(hourlyPrice)} <strong>per hour</strong><br>`;
+                            payloadHTML += `${convertCurrencyToPrettyFormat(monthlyPrice)} <strong>per month</strong><br>`;
+                            payloadHTML += `${convertCurrencyToPrettyFormat(yearlyPrice)} <strong>per year</strong><br></div>`;
+
+                            element.innerHTML = payloadHTML;
+                        }
                     }
                 }
             }
         }
+
+        if (convertedElementCount < 1) {
+            startDelayedFunc();
+        }
+
+        inProgress = false;
+    }
+}
+
+// keeps all decimal places provided to input
+function convertCurrencyToPrettyFormat(num) {
+    let result = '$';
+
+    const strNum = num.toString();
+    const isDecimal = (strNum.indexOf('.') > -1);
+    const wholeNumbers = parseFloat(strNum.substring(0, (isDecimal ? strNum.indexOf('.') : strNum.length))).toLocaleString('en');
+
+    result += wholeNumbers.toString();
+
+    if (isDecimal) {
+        result += strNum.substring(strNum.indexOf('.'), strNum.length);
+
+        // append 0 if decimal place only has one position
+        if (result.length - result.indexOf('.') - 1 === 1) {
+            result += '0';
+        }
     }
 
-    if (convertedElementCount < 1) {
-        startDelayedFunc();
-    }
+    return result;
 }
 
 function startDelayedFunc() {
